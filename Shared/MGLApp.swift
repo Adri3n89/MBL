6e9ddf2ab01c838db066c6eccfc188b17464515f
Adrien <adrien@perea.be>
2021-11-01 10:47:00 +0100

    Initial Commit

---
+++ Shared/MGLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MGLApp.swift
+//  Shared
+//
+//  Created by Adrien PEREA on 01/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MGLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
